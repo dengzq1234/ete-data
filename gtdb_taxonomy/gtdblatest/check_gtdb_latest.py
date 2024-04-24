64c8a280853b5a50cbe51e4b3f973d0f67aa162d
Jordi <jordi.burguet.castell@gmail.com>
2024-04-24 13:18:14 +0200

    check_gtdb_latest.py: Fix it so it works with the latest release of gtdb.
    They changed the names of the files in MD5SUM.txt, and it is quite messy.
    
    At least this script works for now.

--- gtdb_taxonomy/gtdblatest/check_gtdb_latest.py
+++ gtdb_taxonomy/gtdblatest/check_gtdb_latest.py
@@ -9,24 +9,33 @@ information on how to update gtdb_latest_dump.tar.gz if you need to.
 """
 
 import sys
+import re
 import hashlib
 import requests
 
 
 def main():
-    fnames = ['bac120_taxonomy.tsv.gz', 'ar53_taxonomy.tsv.gz']
+    names = ['bac120_taxonomy', 'ar53_taxonomy']
 
-    print('Checking status of files', fnames, '...')
+    print('Checking status of', names, '...')
 
     try:
         md5s_web = get_md5s()
-        for fname in fnames:
-            md5 = hashlib.md5(open(fname, 'rb').read()).hexdigest()
-            print(f'  %22s is up-to-date: %s' % (fname, md5 == md5s_web[fname]))
-    except KeyError as e:
-        sys.exit(f'Cannot find mention of file: {e}')
+        # The names they put in their files don't necessarily
+        # corrspond to the actual files! For example, they can mention
+        # ar53_taxonomy_r220.tsv.gz when the real downloadable file is
+        # ar53_taxonomy.tsv.gz
+
+        for name in names:
+            md5_web = get_md5_web(md5s_web, name)
+            assert md5_web, f'Cannot find mention of file for {name}'
+
+            md5 = hashlib.md5(open(name + '.tsv.gz', 'rb').read()).hexdigest()
+
+            print(f'  %18s is up-to-date: %s' % (name, md5 == md5_web))
+
     except (requests.exceptions.ConnectionError, KeyboardInterrupt,
-            FileNotFoundError, ValueError) as e:
+            FileNotFoundError, ValueError, AssertionError) as e:
         sys.exit(f'Cannot check status: {e}')
 
 
@@ -34,6 +43,14 @@ def get_md5s(url='https://data.gtdb.ecogenomic.org/releases/latest/MD5SUM.txt'):
     """Return a dict like {fname: md5} from parsing the given url."""
     parts = [line.split() for line in requests.get(url).text.splitlines()]
     return {fname.lstrip('./'): md5 for md5, fname in parts}
+
+
+def get_md5_web(md5s_web, name):
+    pattern = name + r'.*\.tsv\.gz'
+    for web_name, md5 in md5s_web.items():
+        if re.match(pattern, web_name):
+            return md5
+    return None
 
 
 
